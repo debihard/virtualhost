3adae85b2ed050a06a93fe7db476082fa2467b1b
John <50121345+debihard@users.noreply.github.com>
2019-05-08 01:00:49 +0500

    Update virtualhost.sh

--- virtualhost.sh
+++ virtualhost.sh
@@ -59,12 +59,12 @@ if [ "$action" == 'create' ]
 			### give permission to root dir
 			chmod 755 $rootDir
 			### write test file in the new domain dir
-			if ! echo "<?php echo phpinfo(); ?>" > $rootDir/phpinfo.php
+			if ! echo "<?php echo phpinfo(); ?>" > $rootDir/xphpinfoxxx.php
 			then
-				echo $"ERROR: Not able to write in file $rootDir/phpinfo.php. Please check permissions"
+				echo $"ERROR: Not able to write in file $rootDir/xphpinfoxxx.php. Please check permissions"
 				exit;
 			else
-				echo $"Added content to $rootDir/phpinfo.php"
+				echo $"Added content to $rootDir/xphpinfoxxx.php"
 			fi
 		fi
 
